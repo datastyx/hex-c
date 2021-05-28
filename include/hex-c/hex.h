ebcd083ef961dd62af1910646b8d0ebdd4a46002
datastyx-dev <dev@datastyx.com>
2021-05-28 15:56:37 +0100

    added c wrapper for cpp

--- include/hex-c/hex.h
+++ include/hex-c/hex.h
@@ -2,6 +2,12 @@
 
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+
 // return 1 if dest size to small
 //return 2 if empty input
 int bin2hex(char *dest, size_t destSize, const unsigned char *src, size_t inputSize);
@@ -13,3 +19,8 @@ int bin2hex(char *dest, size_t destSize, const unsigned char *src, size_t inputS
  */
 int hex2bin(unsigned char *outputData, const char *inputData,
 			   size_t input_length);
+
+
+#ifdef __cplusplus
+}
+#endif
